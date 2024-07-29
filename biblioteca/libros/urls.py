f8f49256bc664ff3b6a8295555aa42fc7bac3be5
Alejandro Ramirez <aleramirez1980@gmail.com>
2024-07-29 00:08:48 -0300

    Base modificada

--- biblioteca/libros/urls.py
+++ biblioteca/libros/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from libros import views
 
 urlpatterns = [
-    path('/', views.inicio, name="Inicio"),
-    path('libros/', views.libro, name="Libros"),
-    path('agregarlibro/', views.agregarlibro, name="Agregar Libro"),
+    path('', views.inicio, name="Inicio"),
+    path('libros', views.libro, name="Libros"),
+    path('agregarlibro', views.agregarlibro, name="Agregar Libro"),
 ]
