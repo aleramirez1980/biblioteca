d9de82e209e5b7b787c599f821ce2715563bb459
Alejandro Ramirez <aleramirez1980@gmail.com>
2024-07-29 00:45:05 -0300

    Commit cambios

--- biblioteca/libros/admin.py
+++ biblioteca/libros/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Libro, Contacto, Estudiante, Bibliotecario
 
 # Register your models here.
+
+admin.site.register(Libro)
+admin.site.register(Estudiante)
+admin.site.register(Contacto)
+admin.site.register(Bibliotecario)
