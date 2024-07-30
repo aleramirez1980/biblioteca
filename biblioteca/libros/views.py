981964e4c057a15802bb69ff504fa9f31d902f98
Alejandro Ramirez <aleramirez1980@gmail.com>
2024-07-29 22:56:55 -0300

    Falta resultados en formulario

--- biblioteca/libros/views.py
+++ biblioteca/libros/views.py
@@ -18,3 +18,20 @@ def agregar_libro(request):
         return render(request, "libros/index.html")
 
     return render(request,"libros/agregar_libro.html")
+
+from libros.forms import LibroFormulario
+
+def busca_libro_con_api(request):
+    if request.method == "POST":
+        mi_formulario = LibroFormulario(request.POST) # Aqui me llega la informacion del html
+
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            
+            libros = Libro.objects.filter(nombre__icontains=informacion["nombre"])
+
+            return render(request, "Libros/mostrar_libros.html", {"libros": libro})
+    else:
+        mi_formulario = LibroFormulario()
+
+    return render(request, "libros/buscar_form_con_api.html", {"mi_formulario": mi_formulario})
